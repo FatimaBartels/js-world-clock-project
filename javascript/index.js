b61f36ab8321af5062a185978b8ad422ee14b380
FatimaBartels <bartels.fatima@gmail.com>
2024-06-13 19:28:03 +0200

    added closing tag to an html element in js file

--- javascript/index.js
+++ javascript/index.js
@@ -38,7 +38,7 @@ function updateCity(event) {
           </div>
           <div class="time">${cityTime.format(
             "h:mm:ss"
-          )} <small>${cityTime.format("A")}</div>
+          )} <small>${cityTime.format("A")}</small></div>
         </div>`;
 }
 
